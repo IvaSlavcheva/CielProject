745a64a53fcf173fbed0da9051305a45d4c8270d
IvaSlavcheva <141230716+IvaSlavcheva@users.noreply.github.com>
2024-02-18 22:21:23 +0200

    18.02

--- Areas/Customer/Controllers/ProductsController.cs
+++ Areas/Customer/Controllers/ProductsController.cs
@@ -6,44 +6,45 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Ciel.Areas.Customer.Controllers
 {
-    [Area("Customer")]
-    public class ProductsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	[Area("Customer")]
+	public class ProductsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public ProductsController(ApplicationDbContext context)
-        {
-            this._context = context;
-        }
-        public async Task<IActionResult> Index(string nameSearch)
-        {
-            ViewData["NameSearch"] = nameSearch;
+		public ProductsController(ApplicationDbContext context)
+		{
+			this._context = context;
+		}
+		public async Task<IActionResult> Index(string nameSearch)
+		{
+			ViewData["NameSearch"] = nameSearch;
 
-            // Get all products with optional filtering by name
-            IQueryable<Product> query = _context.Products
-                .Include(p => p.Catalog);
+			// Get all products with optional filtering by name
+			IQueryable<Product> query = _context.Products
+				.Include(p => p.Catalog);
 
-            if (!string.IsNullOrEmpty(nameSearch))
-            {
-                query = query.Where(p => p.ProductName.Contains(nameSearch));
-            }
-            List<Product> productList = await query.ToListAsync();
-            return View(productList);
-        }
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var product = await _context.Products
-                .Include(p => p.Catalog)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-    }
+			if (!string.IsNullOrEmpty(nameSearch))
+			{
+				query = query.Where(p => p.ProductName.Contains(nameSearch));
+			}
+			List<Product> productList = await query.ToListAsync();
+			return View(productList);
+		}
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+			var product = await _context.Products
+				.Include(p => p.Catalog)
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			return View(product);
+		}
+	}
 }
+
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -30,43 +30,87 @@ namespace Ciel.Data
                 new Product
                 {
                     Id = 1,
-                    ProductName = "Крем за лице",
-                    Description = "Хидратиращ и успокояващ крем",
-                    Picture = "1.jpg",
-                    Price = 19.90,
+                    ProductName = "Dior Addict - олио за устни",
+                    Description = "В днешно време задължителен за гримьорите, Dior Lip Glow се предлага като блестящо масло за устни, което защитава и подобрява структурата на устните в дълбочина, като трайно подчертава естествения им цвят. С технологията Color Reviver, маслото Lip Glow Oil въздейства директно върху нивото на влага на устните, за да създаде желания цвят, като същевременно осигурява трайна хидратация.",
+                    Picture = "image1.jpg",
+                    Price = 77.90,
+                    CatalogId = 3
+                },
+                new Product
+                {
+                    Id = 2,
+                    ProductName = "The Ordinary AHA 30% + BHA 2% - пилинг за лице ",
+                    Description = "Химическият пилинг The Ordinary AHA 30% + BHA 2% Peeling Solution се грижи за ексфолиране на кожата – повърхностно и в дълбочина. Действа с изключително висока интензивност, но благодарение на добавените хидратиращи съставки и на това, че стойностите на pKa и pH са балансирани, минимизира риска от раздразнение.",
+                    Picture = "image2.jpg",
+                    Price = 21,
                     CatalogId = 4
                 },
-
-                 new Product
-                 {
-                     Id = 2,
-                     ProductName = "Серум за мигли",
-                     Description = "Удължава и хидратира миглите",
-                     Picture = "2.jpg",
-                     Price = 17.99,
-                     CatalogId = 2
-                 },
-
-				 new Product
-				 {
-					 Id = 3,
-					 ProductName = "Маска за устни",
-					 Description = "Облекчава сухите и напукани устни.",
-					 Picture = "3.jpg",
-					 Price = 21.99,
+                new Product
+                {
+                    Id = 3,
+                    ProductName = "FILORGA OXYGEN-GLOW - околоочен крем",
+                    Description = "Кремът за сияен околоочен контур Filorga Oxygen-Glow ще направи погледа ви неустоимо бляскав. Той ще ви отърве от уморените очи, ще изглади фините бръчки и ще озари кожата ви по естествен начин.",
+                    Picture = "image3.jpg",
+                    Price = 51.40,
+                    CatalogId = 1
+                },
+				new Product
+				{
+					Id = 4,
+					ProductName = "The Ordinary - серум за вежди и мигли ",
+					Description = "Серумът The Ordinary Multi-Peptide Lash and Brow Serum ще се погрижи за интензивното подхранване на косъмчетата на веждите и миглите. Подпомага укрепването на отделните косъмчета така, че само след 4 седмици ще забележите видими резултати – увеличен обем, плътност и цялостно по-здрав вид на веждите и миглите.",
+					Picture = "image4.jpg",
+					Price = 31,
+					CatalogId = 2
+				},
+				new Product
+				{
+					Id = 5,
+					ProductName = "Dior - слънцезащитен крем",
+                    Description = "Слънцезащитният крем Dior Solar The Protective Creme SPF 30 защитава лицето от UVA/UVB лъчите. Свежата му текстура моментално се разнася и попива в кожата за приятно усещане. След нанасянето кожата е хидратирана, видимо по-красива и подобрена със сатенен блясък. При продължителна употреба тенът става все по-красив.",
+					Picture = "image5.jpg",
+					Price = 80.90,
+					CatalogId = 4
+				},
+			    new Product
+			    {
+					Id = 6,
+					ProductName = "FILORGA - околоочен крем",
+                    Description = "Осигурете на зоната около очите необходимата грижа, която ще я подсили и подмлади. Мултикоригиращият очен крем Filorga NCEF Reverse Eyes бързо попива в кожата, като освобождава уникалния ревитализиращ NCEF комплекс. Той е съставен от 50 активни вещества в такива високи концентрации, каквито се използват в мезотерапиите.",
+					Picture = "image6.jpg",
+					Price = 102.40,
+					CatalogId = 1
+			    },
+				new Product
+			    {
+				     Id = 7,
+					 ProductName = "Clarins - блясък за устни",
+                     Description = "Подчертайте естествената красота на устните си и оптично им придайте обем в една стъпка. Блясъкът за устни с хидратиращ ефект Clarins Lip Make-Up Instant Light с 3D ефект дарява устните с впечатляваща плътност, оцветява ги леко и същевременно ги подхранва.",
+					 Picture = "image7.jpg",
+					 Price = 33.99,
 					 CatalogId = 3
-				 }
-				) ;
-		}
+			    },
+				new Product
+				{
+					Id = 8,
+					ProductName = "L’biotica Repair Lash - серум за растеж на мигли",
+                    Description = "Изхвърлете изкуствените мигли и заложете на естествената красота. Серумът за растеж на миглите L’biotica ActiveLash ги удължава, подсилва и укрепва само след един месец. Активните съставки стимулират растежа на естествените мигли, предотвратяват падането им и удължават фазата им на растеж.",
+					Picture = "image8.jpg",
+					Price = 29.10,
+					CatalogId = 2
+				}
+
+				);
+        }
 
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Catalog> Catalogs { get; set; }
-      
+
         public DbSet<Review> Reviews { get; set; }
-        
+
         public DbSet<Cart> Carts { get; set; }
-        
+
         public DbSet<Cart_Product> Cart_Products { get; set; }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
